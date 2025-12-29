0994c7ca955cf0975f630dac2a6e13cb63b20431
STACiA <stacia.dev@gmail.com>
2025-12-30 01:56:39 +0700

    Initial commit

---
+++ Elizabeth/ElizabethApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElizabethApp.swift
+//  Elizabeth
+//
+//  Created by STACiA on 28/12/2568 BE.
+//
+
+import SwiftUI
+
+@main
+struct ElizabethApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
