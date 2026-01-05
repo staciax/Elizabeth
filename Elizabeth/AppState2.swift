f817c55228a49f126eb5b39086535e7abfe7314b
STACiA <stacia.dev@gmail.com>
2026-01-05 23:25:00 +0700

    fix: add comment for checklist

--- Elizabeth/AppState2.swift
+++ Elizabeth/AppState2.swift
@@ -14,6 +14,7 @@ import SwiftUI
     var collections: [CollectionItem] = []
 
     init() {
+        // collection types: Array - Appending an Array
         collections.append(contentsOf: [
             .collection(
                 id: UUID(),
